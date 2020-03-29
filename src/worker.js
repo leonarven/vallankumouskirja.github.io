e55974a18bb583ce24a5c88007fa2701dae756fa
Miro Nieminen <leonarven@gmail.com>
2020-03-29 16:27:49 +0300

    Vaihdettu haku käyttämään SearchString-luokkaa. Luovutaan :n käytöstä ja vaihdetaan :iin.

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,62 @@
 (() => {
+	class SearchString{
+		constructor( argv ){
+			this.$string   = "";
+			this.$contains = {}
+
+			argv && this.extend( argv );
+		}
+
+		extend( argv ){
+			var str = null;
+			if (typeof argv == "object") {
+				str = "";
+				for (var key in argv) {
+					if (argv[ key ] != null) {
+						argv[ key ] = SearchString.filterString2searchKey( argv[ key ]);
+						this.$contains[ key ] = (this.$contains[ key ] || "") + " " + argv[ key ];
+						str += " " + argv[ key ];
+					}
+				}
+			} else if (typeof argv == "string" && argv.trim().length > 0 ){
+				str = argv;
+			}
+
+			if (typeof str == "string") {
+				this.$string = SearchString.filterString2searchKey( this.$string, str );
+			} else if (argv) {
+				console.warn( "SearchString.extend :: Invalid argument" );
+			}
+		}
+
+		toString() {
+			return this.$string;
+		}
+	}
+
+	SearchString.filterString2searchKey = (function(){
+		var regexp_torm  = /[\.,:'\(\)\[\]\{\}\/\\]/g;  // Poistettavat (whitespacella korvattavat) merkit
+		var regexp_toosm = /\s.{0,3}\s/g;                 // Liian lyhyet whitespacen ympäröimät merkkijonot ( 0-3 merkkiä) (myös whitespacen, (2-5 merkkiä))
+
+		return function( str ) {
+			if (arguments.length > 1) return SearchString.filterString2searchKey( Array.prototype.slice.call( arguments ));
+			if (Array.isArray( str )) str = parseArray2string( str );
+			if (typeof str != "string") return console.warn( "SearchString.filterString2searchkey :: Invalid argument", typeof str, str ), "";
+
+			return (" " + str.toLowerCase().replace( regexp_torm, " " ) + " ").replace( regexp_toosm, " " ).trim();
+		};
+
+		function parseArray2string( arr ){
+			return arr.filter(function( v ) {
+				return !!v;
+			}).map(function( v ) {
+				return Array.isArray( v ) ? parseArray2string( v ) : v;
+			}).join(" ");
+		}
+	})();
+
+
+
 	angular.module("laulukirja-app", [ "ui.router" ])
 
 	.config([ "$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider){
@@ -53,23 +111,16 @@
 				}
 			},
 			resolve : {
-				"$songMeta" : ["$rootScope", "$http", "$stateParams", "$state", "songsIndex", function($rootScope, $http, $stateParams, $state, songsIndex){
-					if (!songsIndex[$stateParams.song_key]) {
+				"$songMeta" : [ "$stateParams", "$state", "songsIndex", function( $stateParams, $state, songsIndex ) {
+					if (!songsIndex[ $stateParams.song_key ]) {
 						console.error("Could not found song '"+ $stateParams.song_key +"'");
 						$state.go("index");
 					}
-					return songsIndex[$stateParams.song_key];
+					return songsIndex[ $stateParams.song_key ];
 				}],
-				"$songBody" : ["$rootScope", "$http", "$stateParams", "$state", "$songMeta", function($rootScope, $http, $stateParams, $state, $songMeta){
-					return $http({
-						url    : "songs/"+ $songMeta.key +"/song.html",
-						method : "GET"
-					}).then(response => {
-						$rootScope.$songMeta = $songMeta;
-						return response.data;
-					}).catch(err => {
-						console.error( "Could not GET 'songs/"+ $songMeta.key +"/song.html'", err );
-						$state.go( "index" );
+				"$songBody" : [ "$templateRequest", "$songMeta", function( $templateRequest, $song ){
+					return $templateRequest( $song.$templateUrl ).catch(err => {
+						console.error( `Could not GET '${ $song.$templateUrl }'`, err );
 					});
 				}]
 			}
@@ -143,7 +194,7 @@
 				if (search) {
 					songsIndex[ key ].$$filtered = true;
 					search.forEach( str => {
-						if (songsIndex[ key ].$search.indexOf( str ) != -1) {
+						if (songsIndex[ key ].$search.$string.indexOf( str ) != -1) {
 							songsIndex[ key ].$$filtered = false;
 						}
 					});
@@ -163,13 +214,12 @@
 	}])
 
 	.controller("songViewController", ["$rootScope", "$scope", "$sce", "songsIndex", "$songMeta", "$songBody", "$timeout", function( $rootScope, $scope, $sce, songsIndex, $songMeta, $songBody, $timeout ) {
-		var scrollStalker;
+		console.log( "songViewController :: Loaded song " + $songMeta.title, $songMeta );
 
 		$scope.$songMeta = $songMeta;
 		$scope.$songBody = $songBody;
 
 		var songBodyElem = document.getElementById("song-body");
-		songBodyElem.innerHTML = $songBody;
 
 		if (window.innerWidth <= 768) $rootScope.songlist.open = false;
 
@@ -220,7 +270,12 @@
 			this.key = key;
 			for (var v in data) this[ v ] = data[ v ];
 
-			this.$search = this.title.toLowerCase();
+			this.$templateUrl = `songs/${ key }/song.html`;
+
+			this.$search = new SearchString({
+				title : data.title || "",
+				num   : data.num == null ? "" : data.num.toString().trim()
+			});
 		}
 	}
 })();
